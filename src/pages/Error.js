22088f20439bc4fdc88b6676b78c10159b3b96e5
Etukudoh Marvellous <Marvellous Etukudoh>
2022-01-11 17:35:53 +0100

    changed some errors

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
+
 const Error = () => {
     return (
         <>
-            <h1>Error, Page not found</h1>
+        <section 
+        className="error" 
+        style={{
+            display: "flex",
+            alignItems:"center",
+            justifyContent:"center",
+            flexDirection:"column"
+        }}>
+        <h1>Error, Page not found</h1>
+        <Link 
+        to="/" 
+        style={{
+            backgroundColor: "#00b392",
+            padding:10, 
+            color:"white"
+            }}>
+                Homepage
+            </Link>
+        </section>
         </>
     )
 }
