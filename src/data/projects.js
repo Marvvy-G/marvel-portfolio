0b0a1af3b7c8f7c9a937f3e6d66a83e073610bed
Etukudoh Marvellous <Marvellous Etukudoh>
2022-09-15 01:19:56 +0100

    added new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,25 +4,41 @@ export const projects =[
     {
         id: uuidv4(),
         title: "Mervert Boutique",
-        desc: "Lorem ipsum dolor sit amet consecteur adipiscing elit",
+        desc: "An e-commerce solution. This is a sophisticated store front fashion tech solution. It allows customers to shop from the comfort of their home ",
         github: "https://github.com/Marvvy-G",
         demo: "https://mervertboutique.com.ng",
         image: "./images/Project-1.jpeg"
     },
     {
         id: uuidv4(),
-        title: "Yelp Camp",
-        desc: "Lorem ipsum dolor sit amet consecteur adipiscing elit",
-        github: "https://github.com/Marvvy-G",
-        demo: "https://mervertboutique.com.ng",
-        image: "./images/Project-2.jpg"
+        title: "Meakaas",
+        desc: "A simple solution to a business website",
+        github: "https://github.com/Marvvy-G/MeakaaV2final",
+        demo: "https://calm-crag-04811.herokuapp.com/",
+        image: "https://images.pexels.com/photos/3912477/pexels-photo-3912477.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: uuidv4(),
+        title: "Bubmaq-g",
+        desc: "A simple solution to a business website",
+        github: "https://github.com/Marvvy-G/BubMaq-G",
+        demo: "https://mighty-peak-90783.herokuapp.com/",
+        image: "https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
     {
         id: uuidv4(),
         title: "Marvvy Eats",
-        desc: "Lorem ipsum dolor sit amet consecteur adipiscing elit",
-        github: "https://github.com/Marvvy-G",
-        demo: "https://mervertboutique.com.ng",
-        image: "./images/Project-3.jpg"
+        desc: "This is a tech in food industry solution. This project solves the problem of food accessibility. It gives food vendor a platform to advertise their delicacies and allows them to meet hungry customers. Please view on laptop",
+        github: "https://github.com/Marvvy-G/Eatery-Application",
+        demo: "https://marvvyeats.herokuapp.com/",
+        image: "https://images.pexels.com/photos/1483769/pexels-photo-1483769.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id: uuidv4(),
+        title: "Tech Clinic",
+        desc: "Bringing Bioinformatics to life. This project inspired by an aspect of biomedical engineering is both a health solution and an administrative solution in the health industry. It allows clinician to carry out their daily activities with ease and speed",
+        github: "https://github.com/Marvvy-G/EHR-full-Application-with-front-end",
+        demo: "https://techclinic.herokuapp.com/transitpage",
+        image: "https://images.pexels.com/photos/4031688/pexels-photo-4031688.jpeg?auto=compress&cs=tinysrgb&w=600"
     }
 ]
