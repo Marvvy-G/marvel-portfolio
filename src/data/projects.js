6c5d8c9019a84dfb3863b5cc2b1ce006a212e71c
Etukudoh Marvellous <Marvellous Etukudoh>
2023-02-28 22:40:26 +0100

    Commiting

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,22 @@
 import {v4 as uuidv4} from "uuid"
 
 export const projects =[
-    {
-        id: uuidv4(),
-        title: "Mervert Boutique",
-        desc: "An e-commerce solution. This is a sophisticated store front fashion tech solution. It allows customers to shop from the comfort of their home ",
-        github: "https://github.com/Marvvy-G",
-        demo: "#",
-        image: "./images/Project-1.jpeg"
-    },
-    {
-        id: uuidv4(),
-        title: "Meakaas",
-        desc: "A simple solution to a business website",
-        github: "https://github.com/Marvvy-G/MeakaaV2final",
-        demo: "https://calm-crag-04811.herokuapp.com/",
-        image: "https://images.pexels.com/photos/3912477/pexels-photo-3912477.jpeg?auto=compress&cs=tinysrgb&w=600"
-    },
+    // {
+    //     id: uuidv4(),
+    //     title: "Mervert Boutique",
+    //     desc: "An e-commerce solution. This is a sophisticated store front fashion tech solution. It allows customers to shop from the comfort of their home ",
+    //     github: "https://github.com/Marvvy-G",
+    //     demo: "#",
+    //     image: "./images/Project-1.jpeg"
+    // },
+    // {
+    //     id: uuidv4(),
+    //     title: "Meakaas",
+    //     desc: "A simple solution to a business website",
+    //     github: "https://github.com/Marvvy-G/MeakaaV2final",
+    //     demo: "https://calm-crag-04811.herokuapp.com/",
+    //     image: "https://images.pexels.com/photos/3912477/pexels-photo-3912477.jpeg?auto=compress&cs=tinysrgb&w=600"
+    // },
     {
         id: uuidv4(),
         title: "Bubmaq-g",
